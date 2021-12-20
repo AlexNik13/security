ed85e0df2baee4420b18fc7085044dc3bc8bcf85
AlexNik13 <4pnav21@gmail.com>
2021-12-20 16:25:20 +0200

    Initial commit

---
+++ src/main/java/com/example/security/student/Student.java
@@ -0,0 +1,25 @@
+package com.example.security.student;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class Student {
+
+    private final Integer studentId;
+    private final String studentName;
+
+    public Student(Integer studentId,
+                   String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+}
